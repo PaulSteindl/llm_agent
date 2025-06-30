559330edc726739ccd46bf7560af3d3de3f3fc0e
PaulSteindl <paul.steindl@live.com>
2025-06-30 20:23:17 +0200

    Finished up Agent Added feedback loop

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
 MAX_CHARS = 10000
+MAX_ITERATIONS = 20
+
+LLM_MODEL = "gemini-2.0-flash-001"
+
+SYSTEM_PROMPT = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- Read file contents
+- List files and directories
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory and a string. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from config import LLM_MODEL, MAX_ITERATIONS, SYSTEM_PROMPT
 from google.genai import types
 from functions.call_function import call_function
 from functions.get_file_content import schema_get_file_content
@@ -29,19 +30,6 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
 ]
 
-system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- Read file contents
-- List files and directories
-- Execute Python files with optional arguments
-- Write or overwrite files
-
-All paths you provide should be relative to the working directory and a string. You must not specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-
 available_functions = types.Tool(
     function_declarations=[
         schema_get_file_content,
@@ -52,28 +40,37 @@ available_functions = types.Tool(
 )
 
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model = "gemini-2.0-flash-001", 
-    contents = messages,
-    config = types.GenerateContentConfig(
-        tools=[available_functions],
-        system_instruction = system_prompt
+
+for iteration in range(MAX_ITERATIONS):
+    response = client.models.generate_content(
+        model = LLM_MODEL, 
+        contents = messages,
+        config = types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction = SYSTEM_PROMPT
+            )
         )
-    )
 
-if response.function_calls:
-    for function_call in response.function_calls:
-        function_call_result = call_function(function_call, verbose)
-        
-        if (not function_call_result.parts or 
-            not function_call_result.parts[0].function_response or 
-            not function_call_result.parts[0].function_response.response):
-            raise Exception("Error: No Result for called function")
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
 
-        if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-else:
-    print(response.text)
+    if response.function_calls:
+        for function_call in response.function_calls:
+            function_call_result = call_function(function_call, verbose)
+            
+            if (not function_call_result.parts or 
+                not function_call_result.parts[0].function_response or 
+                not function_call_result.parts[0].function_response.response):
+                raise Exception("Error: No Result for called function")
+
+            messages.append(function_call_result)
+
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+    else:
+        print(response.text)
+        break
 
 if verbose:
     print(f"User prompt: {prompt}")
